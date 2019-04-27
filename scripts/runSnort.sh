7a3f2f1ecb5672bbf4bb9deebb4d5220b7147a1f
Connor Patsel <therixy@gmail.com>
2019-04-27 12:48:31 -0400

    Update runSnort.sh

--- scripts/runSnort.sh
+++ scripts/runSnort.sh
@@ -2,5 +2,5 @@
 
 
 #run snort on monitor pcap from tcpdump. 
-snort -A console -q -u snort -g snort -c /etc/snort/snort.conf -i enp0s8 -r /var/log/capaguardMonitor.pcap > /var/log/capaguardSnortResult
+snort -A console -q -u snort -g snort -c /etc/snort/snort.conf -i enp0s8 -r /var/log/capaguardMonitor.pcap >| /var/log/capaguardSnortResult
 
